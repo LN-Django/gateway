474d18eb94d478d0c0e68de7c46411ff50432065
Louis Andrew <louisandrew3@gmail.com>
2021-12-28 14:56:23 +0100

    feat(gateway): proxy product list and product info from application service

--- boilerplate/api_gateway/external_services.py
+++ boilerplate/api_gateway/external_services.py
@@ -1,3 +1,5 @@
+
+
 class ExternalService:
     def __init__(self, base_url: str, endpoint: str) -> None:
         self.endpoint = base_url + '/api/' + endpoint
@@ -5,7 +7,7 @@ class ExternalService:
 
 
 services = {
-    'list_products': ExternalService("", ""),
-    'product_info': ExternalService("", ""),
+    'list_products': ExternalService(base_url="https://enigmatic-dusk-38395.herokuapp.com", endpoint="products"),
+    'product_info': ExternalService(base_url="https://enigmatic-dusk-38395.herokuapp.com", endpoint="product/<product_id>/info"),
     'calculator': ExternalService(base_url="https://cryptic-wildwood-57466.herokuapp.com", endpoint="calculator")
 }
--- boilerplate/api_gateway/urls.py
+++ boilerplate/api_gateway/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.urls import path
 
-from .view import CalculatorView
+from .view import CalculatorView, ListProductsView, ProductInfoView
 
 urlpatterns = [
-    url('api/calculator', CalculatorView.as_view())
+    url('api/calculator', CalculatorView.as_view()),
+    path('api/product/<int:product_id>/info',
+         ProductInfoView.as_view()),
+    url('api/products', ListProductsView.as_view()),
 ]
